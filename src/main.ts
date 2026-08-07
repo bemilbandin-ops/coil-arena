2c6f35597a7448d91c2e09db007ad82a74d018bb
bemilbandin-ops <bemilbandin@gmail.com>
2026-08-07 17:44:56 +0200

    Round Phaser display scaling to reduce canvas softness

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ const config: Phaser.Types.Core.GameConfig = {
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
+    autoRound: true,
     width: 1280,
     height: 720,
   },
